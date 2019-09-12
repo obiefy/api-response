da2ec7e4194ba38a339eaf50bad8c423e20de7e4
Obiefy <obayhamed@gmail.com>
2019-09-12 14:09:44 +0400

    enhance validation response

--- tests/ValidationResponseTest.php
+++ tests/ValidationResponseTest.php
@@ -19,11 +19,24 @@ class ValidationResponseTest extends TestCase
     /** @test */
     public function it_returns_validation_failed_response_with_passed_data()
     {
-        $response = api()->validation(['name' => 'name field is required'])->getContent();
+        $response = api()->validation('Name field is required, please check and try again.', ['name' => 'name field is required'])->getContent();
         $expectedResponse = [
-            'MESSAGE' => 'Validation Failed please check the request attributes and try again.',
+            'MESSAGE' => 'Name field is required, please check and try again.',
             'STATUS'  => 402,
             'DATA'    => ['name' => 'name field is required'],
+        ];
+
+        $this->assertEquals($expectedResponse, json_decode($response, 1));
+    }
+
+    /** @test */
+    public function it_returns_validation_response_with_default_data()
+    {
+        $response = api()->validation()->getContent();
+        $expectedResponse = [
+            'STATUS'  => 402,
+            'MESSAGE' => config('api.messages.validation'),
+            'DATA'    => [],
         ];
 
         $this->assertEquals($expectedResponse, json_decode($response, 1));
