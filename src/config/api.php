c803821c32c285c3f9d4a4b989189f66c3df7c91
Beau Raines <beau.raines@gmail.com>
2020-01-04 17:11:28 -0800

    Adds data count to the response
    * appends the `dataCount` for $data that is countable and not empty
    * Adds configuration
    	* configurable label for dataCount, defaulting to "DATACOUNT"
    	* optionally includes the dataCount, defaulting to false
    * Updates to the README
    	* Adds configuation sections
    	* Adds information about matching Status codes (#16)
    	* updates validation status code to 422 to match the changes introduced in fd90e10
    	* Adds data count feature

--- src/config/api.php
+++ src/config/api.php
@@ -2,54 +2,60 @@
 
 return [
 
-    /*
+  /*
      * Turn to string the status code in the json response's body.
      */
-    'stringify' => true,
+  'stringify' => true,
 
-    /*
+  /*
      * Set the status code from the json response to be the same as the status code
      * in the json response's body.
      */
-    'matchstatus' => false,
+  'matchstatus' => false,
 
-    /*
+  /*
+     * Include the count of the "data" in the JSON response
+     */
+  'includeDataCount' => false,
+
+  /*
      * Json response's body labels.
      */
-    'keys'      => [
-        'status'  => 'STATUS',
-        'message' => 'MESSAGE',
-        'data'    => 'DATA',
-    ],
+  'keys'      => [
+    'status'  => 'STATUS',
+    'message' => 'MESSAGE',
+    'data'    => 'DATA',
+    'dataCount' => 'DATACOUNT',
+  ],
 
-    /*
+  /*
      * Default included status codes.
      */
-    'codes' => [
-        'success'    => 200,
-        'notfound'   => 404,
-        'validation' => 422,
-        'error'      => 500,
-    ],
+  'codes' => [
+    'success'    => 200,
+    'notfound'   => 404,
+    'validation' => 422,
+    'error'      => 500,
+  ],
 
-    /*
+  /*
      * Status codes default messages.
      */
-    'messages' => [
-        'success'    => 'Process is successfully completed',
-        'notfound'   => 'Sorry no results query for your request.',
-        'validation' => 'Validation Failed please check the request attributes and try again.',
-        'error'      => 'Server error, please try again later',
-    ],
+  'messages' => [
+    'success'    => 'Process is successfully completed',
+    'notfound'   => 'Sorry no results query for your request.',
+    'validation' => 'Validation Failed please check the request attributes and try again.',
+    'error'      => 'Server error, please try again later',
+  ],
 
-    /*
+  /*
      * Extra methods
      */
-    'methods' => [
-        [
-            'code'    => 403,
-            'method'  => 'forbidden',
-            'message' => 'default message',
-        ],
+  'methods' => [
+    [
+      'code'    => 403,
+      'method'  => 'forbidden',
+      'message' => 'default message',
     ],
+  ],
 ];
