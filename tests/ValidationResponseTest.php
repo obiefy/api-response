dbedb16c246d4b19432364c159f0a3e418c4abbf
Beau Raines <beau.raines@gmail.com>
2020-01-05 18:25:49 -0800

    Updates Validation Unit tests
    Updates the expected status code to match the changes from fd90e10

--- tests/ValidationResponseTest.php
+++ tests/ValidationResponseTest.php
@@ -4,41 +4,41 @@ namespace Obiefy\API\Tests;
 
 class ValidationResponseTest extends TestCase
 {
-    /** @test */
-    public function it_returns_validation_failed_response()
-    {
-        $response = api()->validation()->getContent();
-        $expectedResponse = [
-            'MESSAGE' => 'Validation Failed please check the request attributes and try again.',
-            'STATUS'  => 402,
-            'DATA'    => [],
-        ];
-        $this->assertEquals($expectedResponse, json_decode($response, 1));
-    }
+  /** @test */
+  public function it_returns_validation_failed_response()
+  {
+    $response = api()->validation()->getContent();
+    $expectedResponse = [
+      'MESSAGE' => 'Validation Failed please check the request attributes and try again.',
+      'STATUS'  => 422,
+      'DATA'    => [],
+    ];
+    $this->assertEquals($expectedResponse, json_decode($response, 1));
+  }
 
-    /** @test */
-    public function it_returns_validation_failed_response_with_passed_data()
-    {
-        $response = api()->validation('Name field is required, please check and try again.', ['name' => 'name field is required'])->getContent();
-        $expectedResponse = [
-            'MESSAGE' => 'Name field is required, please check and try again.',
-            'STATUS'  => 402,
-            'DATA'    => ['name' => 'name field is required'],
-        ];
+  /** @test */
+  public function it_returns_validation_failed_response_with_passed_data()
+  {
+    $response = api()->validation('Name field is required, please check and try again.', ['name' => 'name field is required'])->getContent();
+    $expectedResponse = [
+      'MESSAGE' => 'Name field is required, please check and try again.',
+      'STATUS'  => 422,
+      'DATA'    => ['name' => 'name field is required'],
+    ];
 
-        $this->assertEquals($expectedResponse, json_decode($response, 1));
-    }
+    $this->assertEquals($expectedResponse, json_decode($response, 1));
+  }
 
-    /** @test */
-    public function it_returns_validation_response_with_default_data()
-    {
-        $response = api()->validation()->getContent();
-        $expectedResponse = [
-            'STATUS'  => 402,
-            'MESSAGE' => config('api.messages.validation'),
-            'DATA'    => [],
-        ];
+  /** @test */
+  public function it_returns_validation_response_with_default_data()
+  {
+    $response = api()->validation()->getContent();
+    $expectedResponse = [
+      'STATUS'  => 422,
+      'MESSAGE' => config('api.messages.validation'),
+      'DATA'    => [],
+    ];
 
-        $this->assertEquals($expectedResponse, json_decode($response, 1));
-    }
+    $this->assertEquals($expectedResponse, json_decode($response, 1));
+  }
 }
