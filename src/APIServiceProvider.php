edb7aa25e11303f75dba513bd3fbbb56be8e5010
Obiefy <obayhamed@gmail.com>
2020-01-06 17:13:09 +0400

    Update to match StyleCI

--- src/APIServiceProvider.php
+++ src/APIServiceProvider.php
@@ -4,12 +4,11 @@ namespace Obiefy\API;
 
 use Illuminate\Support\ServiceProvider;
 
-class APIServiceProvider extends ServiceProvider {
-
+class APIServiceProvider extends ServiceProvider
+{
     public function register()
     {
-        $this->app->bind('api.response', function ()
-        {
+        $this->app->bind('api.response', function () {
             return new APIResponse();
         });
     }
@@ -21,22 +20,20 @@ class APIServiceProvider extends ServiceProvider {
         $this->registerHelpers();
 
         $this->publishes([
-            __DIR__ . '/config/api.php' => config_path('api.php'),
+            __DIR__.'/config/api.php' => config_path('api.php'),
         ], 'api-response');
     }
 
     protected function setupConfig()
     {
-        $path = realpath($raw = __DIR__ . '/config/api.php') ?: $raw;
+        $path = realpath($raw = __DIR__.'/config/api.php') ?: $raw;
         $this->mergeConfigFrom($path, 'api');
     }
 
     protected function registerHelpers()
     {
-        if (file_exists($helperFile = __DIR__ . '/helpers.php'))
-        {
+        if (file_exists($helperFile = __DIR__.'/helpers.php')) {
             require_once $helperFile;
         }
     }
-
 }
