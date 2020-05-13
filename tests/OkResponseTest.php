bd1862fe615699479487e6faaf993328d899a95c
Obay Hamed <obayhamed@gmail.com>
2020-05-14 00:32:40 +0400

    Add OK helper function

--- tests/OkResponseTest.php
+++ tests/OkResponseTest.php
@@ -32,6 +32,18 @@ class OkResponseTest extends TestCase
     }
 
     /** @test */
+    public function it_returns_ok_response_from_ok_helper_function()
+    {
+        $response = ok('this is message', [])->getContent();
+        $expectedResponse = [
+            'MESSAGE' => 'this is message',
+            'STATUS'  => 200,
+            'DATA'    => [],
+        ];
+        $this->assertEquals($expectedResponse, json_decode($response, 1));
+    }
+
+    /** @test */
     public function it_returns_ok_response_with_default_message()
     {
         $response = api()->ok()->getContent();
